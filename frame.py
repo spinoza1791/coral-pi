f1acf3d25389dab68f8b977bf6dacd457e4c4582
ANDREW CRATON <andrew.craton@gmail.com>
2019-03-29 15:22:08 -0500

    Update frame.py

--- frame.py
+++ frame.py
@@ -39,10 +39,10 @@ while DISPLAY.loop_running():
     track.draw()
     #traject_list[:20,:] = np.array([[i**2 * 0.001212, 23- (i+j)*0.15, i*0.1] for i in range(20)])
     #traject_list[20:,:] = traject_list[19,:]
-    traject_list[:0,:] = np.array([25, 0, 0])
-    traject_list[:1,:] = np.array([50, 0, 0])
-    traject_list[:2,:] = np.array([75, 0, 0])
-    traject_list[:3,:] = np.array([100, 0, 0])
+    traject_list[:0,:] = np.array([25, 10, 10])
+    traject_list[:1,:] = np.array([50, 10, 10])
+    traject_list[:2,:] = np.array([75, 10, 10])
+    traject_list[:3,:] = np.array([100, 10, 10])
     #traject_list[20:,:] = traject_list[19,:]
     #j += 0.01
     track.buf[0].re_init(traject_list)
