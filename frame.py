17d967cc27a3152b09fb8ef7b4ea1a2001576f3a
ANDREW CRATON <andrew.craton@gmail.com>
2019-03-29 15:10:49 -0500

    Update frame.py

--- frame.py
+++ frame.py
@@ -22,16 +22,16 @@ preview_mid_Y = int(screen_H/2 - preview_H/2)
 traject_list = np.array([[i*0.1, i*0.1, i*0.1] for i in range(1000)])
 
 #DISPLAY = pi3d.Display.create(x=50, y=50)
-DISPLAY = pi3d.Display.create(preview_mid_X, preview_mid_Y, w=preview_W, h=preview_H, layer=1, frames_per_second=60)
+DISPLAY = pi3d.Display.create(preview_mid_X, preview_mid_Y, w=preview_W, h=preview_H, layer=1, frames_per_second=30)
 DISPLAY.set_background(0.0, 0.0, 0.0, 0.0) # transparent
 
 keybd = pi3d.Keyboard()
 txtshader = pi3d.Shader("uv_flat")
 linshader = pi3d.Shader('mat_flat')
-CAMERA = pi3d.Camera(is_3d=False)
+#CAMERA = pi3d.Camera(is_3d=False)
 
 tracksh = pi3d.Shader("mat_flat")
-track = pi3d.Lines(vertices=traject_list, material=(1.0,0.0,1.0), z=5.0, line_width=4)
+track = pi3d.Lines(vertices=traject_list, material=(1.0,0.0,1.0), z=1.0, line_width=4)
 track.set_shader(tracksh)
 j = 0.0
 while DISPLAY.loop_running():
