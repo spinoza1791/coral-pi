81e0a21e020ac7b16dfad5ec0b0d87b99cedabc7
ANDREW CRATON <andrew.craton@gmail.com>
2019-03-29 16:13:35 -0500

    Update frame.py

--- frame.py
+++ frame.py
@@ -22,7 +22,6 @@ preview_mid_Y = int(screen_H/2 - preview_H/2)
 #traject_list = np.array([[i*0.1, i*0.1, i*0.1] for i in range(1000)])
 traject_list = np.array([100, 100, 100])
 
-
 #DISPLAY = pi3d.Display.create(x=50, y=50)
 DISPLAY = pi3d.Display.create(preview_mid_X, preview_mid_Y, w=preview_W, h=preview_H, layer=1, frames_per_second=30)
 DISPLAY.set_background(0.0, 0.0, 0.0, 0.0) # transparent
@@ -38,9 +37,9 @@ track.set_shader(tracksh)
 #j = 0.0
 while DISPLAY.loop_running():
     track.draw()
-    #traject_list[:20,:] = np.array([[i**2 * 0.001212, 23- (i+j)*0.15, i*0.1] for i in range(20)])
+    traject_list[:20,:] = np.array([[i**2 * 0.001212, 23- (i+j)*0.15, i*0.1] for i in range(20)])
     #traject_list[20:,:] = traject_list[19,:]
-    traject_list[:0,:] = 100 #np.array([25, 50, 1])
+    #traject_list[:0,:] = 100 #np.array([25, 50, 1])
     #traject_list[:1,:] = np.array([50, 10, 10])
     #traject_list[:2,:] = np.array([75, 10, 10])
     #traject_list[:3,:] = np.array([100, 10, 10])
