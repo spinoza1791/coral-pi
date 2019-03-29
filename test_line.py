d65fcad95303a744ecf1c08d4f01e93490b0100d
ANDREW CRATON <andrew.craton@gmail.com>
2019-03-29 16:24:47 -0500

    Update test_line.py

--- test_line.py
+++ test_line.py
@@ -23,7 +23,7 @@ preview_mid_Y = int(screen_H/2 - preview_H/2)
 traject_list = np.array([[i*0.1, i*0.1, i*0.1] for i in range(1000)])
 
 #DISPLAY = pi3d.Display.create(x=50, y=50)
-DISPLAY = pi3d.Display.create(preview_mid_X, preview_mid_Y, w=preview_W, h=preview_H, layer=1, frames_per_second=30, sample=4)
+DISPLAY = pi3d.Display.create(preview_mid_X, preview_mid_Y, w=preview_W, h=preview_H, layer=1, frames_per_second=30, samples=4)
 DISPLAY.set_background(0.0, 0.0, 0.0, 0.0) # transparent
 
 tracksh = pi3d.Shader("mat_flat")
