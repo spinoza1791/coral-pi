9c7fc34beb692d80114564ed0b49f29bc3e86875
ANDREW CRATON <andrew.craton@gmail.com>
2019-03-30 13:34:50 -0500

    Update pi3d_cam.py

--- pi3d_cam.py
+++ pi3d_cam.py
@@ -91,7 +91,7 @@ def main():
                 start_ms = time.time()
                 results = engine.DetectWithInputTensor(input, top_k=max_obj)
                 elapsed_ms = time.time() - start_ms
-                ms = str(int(elapsed_ms*1000))+ "ms"
+                ms = str(int(elapsed_ms*1000))+"ms"
                 ms_txt.draw()
                 ms_txt.quick_change(ms)                
                 #fps_txt.draw()
@@ -119,7 +119,7 @@ def main():
                         bbox = pi3d.Lines(vertices=bbox_vertices, material=(1.0,0.8,0.05), closed=True, line_width=4)
                         bbox.draw()
                         elapsed_ms = time.time() - start_ms
-                        bbox_time = str(int(elapsed_ms*1000)) + "ms"
+                        bbox_time = str(int(elapsed_ms*1000)) +"ms"
                         
                 if keybd.read() == 27:
                     break
