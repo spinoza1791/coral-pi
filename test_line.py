391017ede3bed094a6204993125e69bb4d6cc51d
ANDREW CRATON <andrew.craton@gmail.com>
2019-03-29 19:21:33 -0500

    Update test_line.py

--- test_line.py
+++ test_line.py
@@ -41,7 +41,7 @@ j = 0.0
 while DISPLAY.loop_running():
     track.draw()
     #traject_list[:20,:] = np.array([[i**2 * 0.001212, 2 - (i+j)*0.15, i*0.1] for i in range(20)])
-    traject_list[:20,:] = np.array([[i*20, i*20, i*10] for i in range(20)])
+    traject_list[:20,:] = np.array([[i*20, i*5, i*10] for i in range(20)])
     #traject_list[:20,:] = 40, 200, 200 
     #j += 0.01
     track.buf[0].re_init(traject_list)
