858b3b6922afa101e69bcff8256fb9e84b5af3bc
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-03 13:38:34 -0500

    Update pi3d-numpy-detector-dev.py

--- pi3d-numpy-detector-dev.py
+++ pi3d-numpy-detector-dev.py
@@ -63,15 +63,15 @@ with picamera.PiCamera() as camera:
     camera.start_preview(fullscreen=False, layer=0, window=(preview_mid_X, preview_mid_Y, preview_W, preview_H))
     try:        
         while DISPLAY.loop_running():
+            start_ms = time.time()
             stream = io.BytesIO()
             camera.capture(stream, use_video_port=True, format='bgr')
             stream.truncate()
             stream.seek(0)
             input = np.frombuffer(stream.getvalue(), dtype=np.uint8)
             #stream.close()
-            start_ms = time.time()
-            results = engine.DetectWithInputTensor(input, top_k=max_obj)
             elapsed_ms = time.time() - start_ms
+            results = engine.DetectWithInputTensor(input, top_k=max_obj)
             ms = str(int(elapsed_ms*1000))+"ms"
             ms_txt.draw()
             ms_txt.quick_change(ms)                
