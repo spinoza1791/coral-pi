0cd89d95bb1102fc99d7404dcfab5a6c14e8f5d7
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-02 21:19:12 -0500

    Update pi3d_cam.py

--- pi3d_cam.py
+++ pi3d_cam.py
@@ -5,13 +5,12 @@ import picamera
 import time
 import pi3d
 import math
-from pi3d.constants import GL_LINE_LOOP
 import tkinter
 import numpy as np
 import argparse
 import io
 import edgetpu.detection.engine
-#def main():
+
 parser = argparse.ArgumentParser()
 parser.add_argument(
   '--model', help='File path of Tflite model.', required=True)
@@ -35,7 +34,6 @@ preview_H = 320
 preview_mid_X = int(screen_W/2 - preview_W/2)
 preview_mid_Y = int(screen_H/2 - preview_H/2)
 
-#Set fps as low as needed (<=60) to prevent CPU usage
 DISPLAY = pi3d.Display.create(preview_mid_X, preview_mid_Y, w=preview_W, h=preview_H, layer=1, frames_per_second=max_fps)
 DISPLAY.set_background(0.0, 0.0, 0.0, 0.0) # transparent
 keybd = pi3d.Keyboard()
@@ -45,30 +43,18 @@ CAMERA = pi3d.Camera(is_3d=False)
 font = pi3d.Font("fonts/FreeMono.ttf", font_size=30, color=(0, 255, 0, 255)) # blue green 1.0 alpha
 x1=x2=x3=x4=x5=y1=y2=y3=y4=y5=z= 1
 bbox_vertices = [[x1,y1,z], [x2,y2,z], [x3,y3,z], [x4,y4,z]] 
-#bbox = pi3d.Lines(vertices=[[x1,y1,z], [x2,y2,z], [x3,y3,z], [x4,y4,z]], line_width=4)
 bbox = pi3d.Lines(vertices=bbox_vertices, material=(1.0,0.8,0.05), closed=True, line_width=4) 
 bbox.set_shader(linshader)
-#fps = "00.00FPS"
-#N = 10
-#fps_txt = pi3d.String(camera=CAMERA, is_3d=False, font=font, string=fps, x=0, y=preview_H/2 - 10, z=1.0)
-#fps_txt.set_shader(txtshader)
+fps = "00.00FPS"
+N = 10
+fps_txt = pi3d.String(camera=CAMERA, is_3d=False, font=font, string=fps, x=0, y=preview_H/2 - 10, z=1.0)
+fps_txt.set_shader(txtshader)
 elapsed_ms = 1/1000
 ms = "00ms"
 ms_txt = pi3d.String(camera=CAMERA, is_3d=False, font=font, string=ms, x=0, y=preview_H/2 - 30, z=1.0)
 ms_txt.set_shader(txtshader)
-bbox_time = pi3d.String(camera=CAMERA, is_3d=False, font=font, string=ms, x=0, y=preview_H/2 - 10, z=1.0)
-bbox_time.set_shader(txtshader)   
-#last_tm = time.time()
-#i = 0
-t_mat = [[1.0, 0.0, 0.0], # translation matrix
-         [0.0, 1.0, 0.0],
-         [0.0, 0.0, 1.0]]
-r_mat = [[1.0, 0.0, 0.0], # rotation matrix
-         [0.0, 1.0, 0.0],
-         [0.0, 0.0, 1.0]]
-s_mat = [[1.0, 0.0, 0.0], # scale matrix - all three start out as 'identity' matrices
-         [0.0, 1.0, 0.0],
-         [0.0, 0.0, 1.0]]
+last_tm = time.time()
+i = 0
 
 def bbox_calc(bbox_x1, bbox_y1, bbox_x2, bbox_y2, mdl_dims):
     #Left - X1
@@ -82,23 +68,6 @@ def bbox_calc(bbox_x1, bbox_y1, bbox_x2, bbox_y2, mdl_dims):
     z = 1
     bbox_vertices = [[x1,y1,z], [x2,y2,z], [x3,y3,z], [x4,y4,z]]
     return bbox_vertices
-
-def mat_vec_mult(mat, vec):
-  """ apply a matrix to a vector and return a modified vector
-  """
-  return [mat[0][0] * vec[0] + mat[0][1] * vec[1] + mat[0][2] * vec[2],
-          mat[1][0] * vec[0] + mat[1][1] * vec[1] + mat[1][2] * vec[2],
-          mat[2][0] * vec[0] + mat[2][1] * vec[1] + mat[2][2] * vec[2]]
-
-def refresh_vertices(shape, old_verts):
-  new_verts = []  # start off with a new list of vectors to keep the original ones 'clean'
-  for v in old_verts:
-    new_v = mat_vec_mult(s_mat, [v[0], v[1], 1.0]) # i.e. x, y, 1.0 to match 3x3 matrix
-    new_v = mat_vec_mult(r_mat, new_v) # then multiply by rotation matrix
-    new_v = mat_vec_mult(t_mat, new_v) # then by translation
-    new_verts.append(new_v) # finally add this to the new list of vectors
-    shape.set_2d_location(
-  shape.re_init(vertices=new_verts) # finally update the vertex locations
 
 with picamera.PiCamera() as camera:
     camera.resolution = (preview_W, preview_H)
@@ -120,16 +89,15 @@ with picamera.PiCamera() as camera:
             ms = str(int(elapsed_ms*1000))+"ms"
             ms_txt.draw()
             ms_txt.quick_change(ms)                
-            #fps_txt.draw()
-            #i += 1
-            #if i > N:
-            #    tm = time.time()
-            #    fps = "{:6.2f}FPS".format(i / (tm - last_tm))
-            #    fps_txt.quick_change(fps)
-            #    i = 0
-            #    last_tm = tm
+            fps_txt.draw()
+            i += 1
+            if i > N:
+                tm = time.time()
+                fps = "{:6.2f}FPS".format(i / (tm - last_tm))
+                fps_txt.quick_change(fps)
+                i = 0
+                last_tm = tm
             if results:
-                #start_ms = time.time()
                 num_obj = 0
                 for obj in results:
                     num_obj = num_obj + 1
@@ -142,13 +110,8 @@ with picamera.PiCamera() as camera:
                     bbox_y2 = round(bbox[3] * mdl_dims)
                     bbox_vertices = bbox_calc(bbox_x1, bbox_y1, bbox_x2, bbox_y2, mdl_dims)
                     #bbox.re_init(vertices=bbox_vertices, material=(1.0,0.8,0.05), closed=True, line_width=4)
-                    #bbox = pi3d.Lines(vertices=bbox_vertices, material=(1.0,0.8,0.05), closed=True, line_width=4)
-                    refresh_vertices(bbox, bbox_vertices)
+                    bbox = pi3d.Lines(vertices=bbox_vertices, material=(1.0,0.8,0.05), closed=True, line_width=4)
                     bbox.draw()
-                    elapsed_ms = time.time() - start_ms
-                    ms = str(int(elapsed_ms*1000))+"ms"
-                    bbox_time.draw()
-                    bbox_time.quick_change(ms) 
 
             if keybd.read() == 27:
                 break
@@ -156,9 +119,6 @@ with picamera.PiCamera() as camera:
         keybd.close()
         DISPLAY.destroy()
         camera.close()
-
-#if __name__ == '__main__':
- #   main()
 
 
 
