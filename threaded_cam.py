6b85c1a12f3d4204b9609da879b3241115d1e677
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-04 17:22:02 -0500

    Update threaded_cam.py

--- threaded_cam.py
+++ threaded_cam.py
@@ -7,14 +7,7 @@ import argparse
 import imutils
 import time
  
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-n", "--num-frames", type=int, default=100,
-	help="# of frames to loop over for FPS test")
-ap.add_argument("-d", "--display", type=int, default=-1,
-	help="Whether or not frames should be displayed")
-args = vars(ap.parse_args())
- 
+
 # created a *threaded *video stream, allow the camera sensor to warmup,
 # and start the FPS counter
 print("[INFO] sampling THREADED frames from `picamera` module...")
