ea913fb135399ea4af03ba62733d66d7702cda17
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-04 16:54:11 -0500

    Update PiVideoStream.py

--- PiVideoStream.py
+++ PiVideoStream.py
@@ -10,11 +10,13 @@ class PiVideoStream:
 		self.camera = PiCamera()
 		self.camera.resolution = resolution
 		self.camera.framerate = framerate
-                self.rawCapture = PiRGBArray(self.camera, size=resolution)
+                self.rgbCapture = bytearray(camera.resolution[0] * camera.resolution[1] * 3)
                 #self.rawCapture = bytearray(camera.resolution[0] * camera.resolution[1] * 3)
                 #_, width, height, channels = engine.get_input_tensor_shape()
-                self.stream = self.camera.capture_continuous(self.rawCapture,
-			format="bgr", use_video_port=True)
+                #self.stream = self.camera.capture_continuous(self.rawCapture,
+		#	format="bgr", use_video_port=True)
+		self.stream = self.camera.capture_continuous(self.rgbCapture,
+			format="rgb", use_video_port=True)
 
 		# initialize the frame and the variable used to indicate
 		# if the thread should be stopped
@@ -32,13 +34,14 @@ class PiVideoStream:
 			# grab the frame from the stream and clear the stream in
 			# preparation for the next frame
                         self.frame = f.array
-                        self.rawCapture.truncate(0)
+                        #self.rawCapture.truncate(0)
 
 			# if the thread indicator variable is set, stop the thread
 			# and resource camera resources
 			if self.stopped:
 				self.stream.close()
-				self.rawCapture.close()
+				#self.rawCapture.close()
+				self.rgbCapture.close()
 				self.camera.stop_preview()
                                 self.camera.close()
 				return
