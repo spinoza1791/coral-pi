65f19597e97cf9db336e6c113c569da66b51cb57
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-04 15:20:50 -0500

    Update face_pygame.py

--- face_pygame.py
+++ face_pygame.py
@@ -48,52 +48,52 @@ def main():
              keys = pygame.key.get_pressed()
              if(keys[pygame.K_ESCAPE] == 1):
                 exitFlag = False
-                
-        stream = io.BytesIO()
-        start_ms = time.time()
-        camera.capture(stream, use_video_port=True, format='rgb')
-        elapsed_ms = time.time() - start_ms
-        stream.seek(0)
-        stream.readinto(rgb)
-        stream.truncate()
-        img = pygame.image.frombuffer(rgb[0:
-        (camera.resolution[0] * camera.resolution[1] * 3)],
-        camera.resolution, 'RGB')
-        input = np.frombuffer(stream.getvalue(), dtype=np.uint8)
-        #Inference
-        results = engine.DetectWithInputTensor(input, top_k=max_obj)
-        stream.close()                                                                 
-        if img:
-             screen.blit(img, (0,0))
-             if results:
-                  num_obj = 0
-                  for obj in results:
-                       num_obj = num_obj + 1
-                  for obj in results:
-                       bbox = obj.bounding_box.flatten().tolist()
-                       score = round(obj.score,2)
-                       x1 = round(bbox[0] * mdl_dims)
-                       y1 = round(bbox[1] * mdl_dims)
-                       x2 = round(bbox[2] * mdl_dims)
-                       y2 = round(bbox[3] * mdl_dims)
-                       rect_width = x2 - x1
-                       rect_height = y2 - y1
-                       class_score = "%.2f" % (score)
-                       ms = "(%d) %s%.2fms" % (num_obj, "faces detected in ", elapsed_ms*1000)
-                       fnt_class_score = myfont.render(class_score, True, (0,0,255))
-                       fnt_class_score_width = fnt_class_score.get_rect().width
-                       screen.blit(fnt_class_score,(x1, y1-fnt_sz))
-                       fnt_ms = myfont.render(ms, True, (255,255,255))
-                       fnt_ms_width = fnt_ms.get_rect().width
-                       screen.blit(fnt_ms,((mdl_dims / 2) - (fnt_ms_width / 2), 0))
-                       bbox_rect = pygame.draw.rect(screen, (0,0,255), (x1, y1, rect_width, rect_height), 2)
-                       #pygame.display.update(bbox_rect)
-             else:
-                  elapsed_ms = time.time() - start_ms
-                  ms = "%s %.2fms" % ("No faces detected in", elapsed_ms*1000)
-                  fnt_ms = myfont.render(ms, True, (255,0,0))
-                  fnt_ms_width = fnt_ms.get_rect().width
-                  screen.blit(fnt_ms,((mdl_dims / 2) - (fnt_ms_width / 2), 0))
+        with picamera.array.PiRGBArray(camera) as output        
+            stream = io.BytesIO()
+            start_ms = time.time()
+            camera.capture(stream, use_video_port=True, format='rgb')
+            elapsed_ms = time.time() - start_ms
+            stream.seek(0)
+            stream.readinto(rgb)
+            stream.truncate()
+            img = pygame.image.frombuffer(rgb[0:
+            (camera.resolution[0] * camera.resolution[1] * 3)],
+            camera.resolution, 'RGB')
+            input = np.frombuffer(stream.getvalue(), dtype=np.uint8)
+            #Inference
+            results = engine.DetectWithInputTensor(input, top_k=max_obj)
+            stream.close()                                                                 
+            if img:
+                 screen.blit(img, (0,0))
+                 if results:
+                      num_obj = 0
+                      for obj in results:
+                           num_obj = num_obj + 1
+                      for obj in results:
+                           bbox = obj.bounding_box.flatten().tolist()
+                           score = round(obj.score,2)
+                           x1 = round(bbox[0] * mdl_dims)
+                           y1 = round(bbox[1] * mdl_dims)
+                           x2 = round(bbox[2] * mdl_dims)
+                           y2 = round(bbox[3] * mdl_dims)
+                           rect_width = x2 - x1
+                           rect_height = y2 - y1
+                           class_score = "%.2f" % (score)
+                           ms = "(%d) %s%.2fms" % (num_obj, "faces detected in ", elapsed_ms*1000)
+                           fnt_class_score = myfont.render(class_score, True, (0,0,255))
+                           fnt_class_score_width = fnt_class_score.get_rect().width
+                           screen.blit(fnt_class_score,(x1, y1-fnt_sz))
+                           fnt_ms = myfont.render(ms, True, (255,255,255))
+                           fnt_ms_width = fnt_ms.get_rect().width
+                           screen.blit(fnt_ms,((mdl_dims / 2) - (fnt_ms_width / 2), 0))
+                           bbox_rect = pygame.draw.rect(screen, (0,0,255), (x1, y1, rect_width, rect_height), 2)
+                           #pygame.display.update(bbox_rect)
+                 else:
+                      elapsed_ms = time.time() - start_ms
+                      ms = "%s %.2fms" % ("No faces detected in", elapsed_ms*1000)
+                      fnt_ms = myfont.render(ms, True, (255,0,0))
+                      fnt_ms_width = fnt_ms.get_rect().width
+                      screen.blit(fnt_ms,((mdl_dims / 2) - (fnt_ms_width / 2), 0))
 
         pygame.display.update()
 
