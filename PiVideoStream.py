e5206af1b798e7d0b1ac99cc18ff9e506c63d8a8
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-04 17:24:53 -0500

    Update PiVideoStream.py

--- PiVideoStream.py
+++ PiVideoStream.py
@@ -4,7 +4,7 @@ from picamera import PiCamera
 from threading import Thread
  
 class PiVideoStream:
-	def __init__(self, resolution=(320, 240), framerate=32):
+	def __init__(self, resolution=(320, 320), framerate=32):
 		# initialize the camera and stream
 		self.camera = PiCamera()
 		self.camera.resolution = resolution
