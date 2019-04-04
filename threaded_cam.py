b34d6206f5a6b16f2174bb70a5193720c7240d73
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-04 17:18:26 -0500

    Update threaded_cam.py

--- threaded_cam.py
+++ threaded_cam.py
@@ -15,14 +15,6 @@ ap.add_argument("-d", "--display", type=int, default=-1,
 	help="Whether or not frames should be displayed")
 args = vars(ap.parse_args())
  
-# initialize the camera and stream
-camera = PiCamera()
-camera.resolution = (320, 240)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(320, 240))
-stream = camera.capture_continuous(rawCapture, format="bgr",
-	use_video_port=True)
-  
 # created a *threaded *video stream, allow the camera sensor to warmup,
 # and start the FPS counter
 print("[INFO] sampling THREADED frames from `picamera` module...")
