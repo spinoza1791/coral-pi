8db258cd0808d4660b1ec15a472b0d6e0d45b736
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-04 13:35:44 -0500

    Update pi3d-numpy-detector-dev.py

--- pi3d-numpy-detector-dev.py
+++ pi3d-numpy-detector-dev.py
@@ -23,7 +23,7 @@ mdl_dims = int(args.dims) #dims must be a factor of 32 for picamera resolut$
 
 #Set max num of objects you want to detect per frame
 max_obj = 10
-max_fps = 20
+max_fps = 24
 engine = edgetpu.detection.engine.DetectionEngine(args.model)
 
 root = tkinter.Tk()
