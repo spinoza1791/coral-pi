51c7c7f6f7dedb8fee9fed65388cb6761966b163
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-05 18:07:16 -0500

    Update pivideostream.py

--- pivideostream.py
+++ pivideostream.py
@@ -12,9 +12,12 @@ class PiVideoStream:
 		self.camera = PiCamera()
 		self.camera.resolution = (320, 320)
 		self.camera.framerate = 24
-		self.rawCapture = PiRGBArray(self.camera, size=(320, 320))
-		self.stream = self.camera.capture_continuous(self.rawCapture,
-			format="bgr", use_video_port=True)
+		#self.rawCapture = PiRGBArray(self.camera, size=(320, 320))
+		#self.stream = self.camera.capture_continuous(self.rawCapture,
+		#	format="bgr", use_video_port=True)
+		self.rgbCapture = bytearray(self.camera.resolution[0] * self.camera.resolution[1] * 3)
+		self.camera.start_preview(fullscreen=False, layer=0, window=(0, 0, 320, 320))
+                time.sleep(2) #camera warm-up
 		self.frame = None
 		self.stopped = False
 
@@ -23,15 +26,16 @@ class PiVideoStream:
 		return self
 
 	def update(self):
-		#self.stream = io.BytesIO()
-		#self.camera.capture(self.stream, use_video_port=True, format='rgb')
+		self.stream = io.BytesIO()
+		self.camera.capture(self.stream, use_video_port=True, format='rgb')
 		#self.stream.truncate()
-		#self.stream.seek(0)
-		#self.stream.readinto(self.rawCapture)
-		for f in self.stream:
-			self.frame = f.array
+		self.stream.seek(0)
+		self.stream.readinto(self.rgbCapture)
+		#for f in self.stream:
+		#	self.frame = f.array
 			#self.input = np.frombuffer(self.frame.getvalue(), dtype=np.uint8)
-			self.rawCapture.truncate(0)
+		#	self.rawCapture.truncate(0)
+		self.input = np.frombuffer(self.stream.getvalue(), dtype=np.uint8)
 		#self.stream.close()
 		if self.stopped:
 			self.stream.close()
@@ -40,7 +44,7 @@ class PiVideoStream:
 			return
 
 	def read(self):
-		return self.frame
+		return self.input
 
 	def stop(self):
 		# indicate that the thread should be stopped
