93146e3c569dc0f8c3600d0aed0679105a170f22
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-05 18:47:40 -0500

    Update pivideostream.py

--- pivideostream.py
+++ pivideostream.py
@@ -11,7 +11,7 @@ class PiVideoStream:
 	def __init__(self):
 		self.camera = PiCamera()
 		self.camera.resolution = (320, 320)
-		self.camera.framerate = 15
+		self.camera.framerate = 24
 		self.rawCapture = PiRGBArray(self.camera, size=(320, 320))
 		#self.rawCapture = PiRGBArray(self.camera, size=(320 * 320 * 3))
 		self.camera.start_preview(fullscreen=False, layer=0, window=(0, 0, 320, 320))
