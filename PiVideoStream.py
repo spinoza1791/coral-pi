70c87dd0f3a788b89bda8bbb9bb5a68df6358ccf
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-05 11:30:31 -0500

    Update PiVideoStream.py

--- PiVideoStream.py
+++ PiVideoStream.py
@@ -6,24 +6,18 @@ import time
  
 class PiVideoStream:
 	def __init__(self):
-		# initialize the camera and stream
 		self.camera = PiCamera()
 		self.rbgCapture = PiRGBArray(self.camera, size=(320, 320))
 		#self.stream = self.camera.capture_continuous(self.rbgCapture,
 		#	format="rgb", use_video_port=True)
 		self.stream = self.camera.capture(self.rbgCapture, use_video_port=True, format='rgb')
- 
-		# initialize the frame and the variable used to indicate
-		# if the thread should be stopped
 		self.frame = None
 		self.stopped = False
 
 	def start(self):
-		# start the thread to read frames from the video stream
-
 		Thread(target=self.update, args=()).start()
 		return self
- 
+
 	def update(self):
 		# keep looping infinitely until the thread is stopped
 		for f in self.stream:
@@ -31,7 +25,6 @@ class PiVideoStream:
 			# preparation for the next frame
 			self.frame = f.array
 			self.rbgCapture.truncate(0)
- 
 			# if the thread indicator variable is set, stop the thread
 			# and resource camera resources
 			if self.stopped:
@@ -39,10 +32,11 @@ class PiVideoStream:
 				self.rbgCapture.close()
 				self.camera.close()
 				return
+
 	def read(self):
 		# return the frame most recently read
 		return self.frame
- 
+
 	def stop(self):
 		# indicate that the thread should be stopped
 		self.stopped = True
