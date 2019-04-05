4139434ee14a071e0a3552851a47e6338a37108b
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-05 11:26:14 -0500

    Update threaded_cam.py

--- threaded_cam.py
+++ threaded_cam.py
@@ -7,6 +7,22 @@ import argparse
 import imutils
 import time
 import tkinter
+import edgetpu.detection.engine
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+  '--model', help='File path of Tflite model.', required=True)
+parser.add_argument(
+  '--dims', help='Model input dimension', required=True)
+args = parser.parse_args()
+
+#Set all input params equal to the input dimensions expected by the model
+mdl_dims = int(args.dims) #dims must be a factor of 32 for picamera resolut$
+
+#Set max num of objects you want to detect per frame
+max_obj = 10
+max_fps = 24
+engine = edgetpu.detection.engine.DetectionEngine(args.model)
 
 root = tkinter.Tk()
 screen_W = root.winfo_screenwidth()
@@ -16,15 +32,38 @@ preview_H = 320
 preview_mid_X = int(screen_W/2 - preview_W/2)
 preview_mid_Y = int(screen_H/2 - preview_H/2)
 
+DISPLAY = pi3d.Display.create(preview_mid_X, preview_mid_Y, w=preview_W, h=preview_H, layer=1, frames_per_second=max_fps)
+DISPLAY.set_background(0.0, 0.0, 0.0, 0.0) # transparent
+keybd = pi3d.Keyboard()
+txtshader = pi3d.Shader("uv_flat")
+
+CAMERA = pi3d.Camera(is_3d=False)
+font = pi3d.Font("fonts/FreeMono.ttf", font_size=30, color=(0, 255, 0, 255)) # blue green 1.0 alpha
+
+elapsed_ms = 1/1000
+ms = "00ms"
+ms_txt = pi3d.String(camera=CAMERA, is_3d=False, font=font, string=ms, x=0, y=preview_H/2 - 30, z=1.0)
+ms_txt.set_shader(txtshader)
+
 stream = PiVideoStream().start()
 stream.camera.resolution = (320, 320)
-stream.camera.framerate = 24
+stream.camera.framerate = max_fps
 stream.camera.start_preview(fullscreen=False, layer=0, window=(preview_mid_X, preview_mid_Y, preview_W, preview_H))
 time.sleep(2.0)
  
 # loop over some frames...this time using the threaded stream
-while True:
-	frame = stream.read()
- 
-# do a bit of cleanup
-stream.stop()
+try: 
+	while DISPLAY.loop_running():
+		start_ms = time.time() 
+		frame = stream.read()
+		elapsed_ms = time.time() - start_ms           
+		ms = str(int(elapsed_ms*100000))+"ms"
+		ms_txt.draw()
+		ms_txt.quick_change(ms)
+		if keybd.read() == 27:
+			break
+			
+finally:
+	keybd.close()
+	DISPLAY.destroy()
+	stream.close()
