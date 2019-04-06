da2d3465616ba09134c9c891b810836c571ee9c0
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-06 13:02:47 -0500

    Update pivideostream.py

--- pivideostream.py
+++ pivideostream.py
@@ -18,6 +18,7 @@ class PiVideoStream:
 		time.sleep(2) #camera warm-up
 		self.stream = self.camera.capture_continuous(self.rawCapture, format="bgr", use_video_port=True)
 		self.frame = None
+		self.frame_buf_val = None
 		self.output = None
 		self.stopped = False
 
