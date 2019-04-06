b0c9210922b5951fc2f27c879157c4c1cffefe40
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-06 13:40:13 -0500

    Update pivideostream.py

--- pivideostream.py
+++ pivideostream.py
@@ -33,7 +33,7 @@ class PiVideoStream:
 		#self.stream.readinto(self.rawCapture)
 		#for f in self.stream:
 		#self.frame = io.BytesIO(f.array)
-		self.frame = io.BytesIO(self.stream)
+		self.frame = io.BytesIO(self.stream.array)
 		self.frame_buf_val = np.frombuffer(self.frame.getvalue(), dtype=np.uint8)
 		self.output = self.engine.DetectWithInputTensor(self.frame_buf_val, top_k=10)
 		self.rawCapture.truncate(0)
