8113e883939f77bd4e094cd10eeb41547f9724a0
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-06 14:22:20 -0500

    Update pivideostream.py

--- pivideostream.py
+++ pivideostream.py
@@ -15,7 +15,7 @@ class PiVideoStream:
 		self.camera.resolution = (320, 320)
 		self.camera.framerate = 24
 		#self.rawCapture = PiRGBArray(self.camera, size=(320, 320))
-		self.rawCapture = bytearray(camera.resolution[0] * camera.resolution[1] * 3)
+		self.rawCapture = bytearray(self.camera.resolution[0] * self.camera.resolution[1] * 3)
 		self.camera.start_preview(fullscreen=False, layer=0, window=(0, 0, 320, 320))
 		time.sleep(2) #camera warm-up
 		#self.stream = self.camera.capture_continuous(self.rawCapture, format="bgr", use_video_port=True)
