bf7228bef9af8755ec2ac66efb8ba2f31c119881
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-06 14:13:09 -0500

    Update pivideostream.py

--- pivideostream.py
+++ pivideostream.py
@@ -18,7 +18,7 @@ class PiVideoStream:
 		#self.rawCapture = bytearray(camera.resolution[0] * camera.resolution[1] *)
 		self.camera.start_preview(fullscreen=False, layer=0, window=(0, 0, 320, 320))
 		time.sleep(2) #camera warm-up
-		self.stream = self.camera.capture_continuous(self.rawCapture, format="bgr", use_video_port=True)
+		#self.stream = self.camera.capture_continuous(self.rawCapture, format="bgr", use_video_port=True)
 		self.frame = None
 		self.frame_buf_val = None
 		self.output = None
@@ -31,12 +31,14 @@ class PiVideoStream:
 	def update(self):
 		#self.stream.seek(0)
 		#self.stream.readinto(self.rawCapture)
+		self.stream = camera.capture(self.rawCapture, use_video_port=True, format='bgr')
 		for f in self.stream:
 			self.frame = io.BytesIO(f.array)
 			self.frame_buf_val = np.frombuffer(self.frame.getvalue(), dtype=np.uint8)
 			self.output = self.engine.DetectWithInputTensor(self.frame_buf_val, top_k=10)
 			self.rawCapture.seek(0)
 			self.rawCapture.truncate()
+		self.stream.close()
 		if self.stopped:
 			self.stream.close()
 			self.rawCapture.close()
