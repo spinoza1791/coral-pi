a1fb92aa07b833f992909602e4da2cfd3d949289
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-06 12:11:59 -0500

    Create stream_thread.py

---
+++ stream_thread.py
@@ -0,0 +1,64 @@
+import io
+import time
+import threading
+import picamera
+
+# Create a pool of image processors
+done = False
+lock = threading.Lock()
+pool = []
+
+class ImageProcessor(threading.Thread):
+    def __init__(self):
+        super(ImageProcessor, self).__init__()
+        self.stream = io.BytesIO()
+        self.event = threading.Event()
+        self.terminated = False
+        self.start()
+
+    def run(self):
+        # This method runs in a separate thread
+        global done
+        while not self.terminated:
+            if self.event.wait(1):
+                try:
+                    self.stream.seek(0)
+                    # Read the image and do some processing on it
+                    #Image.open(self.stream)
+                    #...
+                    #...
+                    # Set done to True if you want the script to terminate
+                    # at some point
+                    #done=True
+                finally:
+                    # Reset the stream and event
+                    self.stream.seek(0)
+                    self.stream.truncate()
+                    self.event.clear()
+                    # Return ourselves to the pool
+                    with lock:
+                        pool.append(self)
+
+def streams():
+    while not done:
+        with lock:
+            processor = pool.pop()
+        yield processor.stream
+        processor.event.set()
+
+with picamera.PiCamera() as camera:
+    pool = [ImageProcessor() for i in range (4)]
+    camera.resolution = (320, 320)
+    # Set the framerate appropriately; too fast and the image processors
+    # will stall the image pipeline and crash the script
+    camera.framerate = 10
+    camera.start_preview()
+    time.sleep(2)
+    camera.capture_sequence(streams(), use_video_port=True)
+
+# Shut down the processors in an orderly fashion
+while pool:
+    with lock:
+        processor = pool.pop()
+    processor.terminated = True
+    processor.join()
