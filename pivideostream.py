778bef0f96a9a1e521829472a39ddb9ebb5877cb
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-06 14:15:44 -0500

    Update pivideostream.py

--- pivideostream.py
+++ pivideostream.py
@@ -32,12 +32,13 @@ class PiVideoStream:
 		#self.stream.seek(0)
 		#self.stream.readinto(self.rawCapture)
 		self.stream = camera.capture(self.rawCapture, use_video_port=True, format='bgr')
+		self.rawCapture.seek(0)
+		self.stream.readinto(self.rawCapture)
 		for f in self.stream:
 			self.frame = io.BytesIO(f.array)
 			self.frame_buf_val = np.frombuffer(self.frame.getvalue(), dtype=np.uint8)
 			self.output = self.engine.DetectWithInputTensor(self.frame_buf_val, top_k=10)
-			self.rawCapture.seek(0)
-			self.rawCapture.truncate()
+			#self.rawCapture.truncate()
 		self.stream.close()
 		if self.stopped:
 			self.stream.close()
