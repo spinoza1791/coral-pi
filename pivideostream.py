6aa2b2ecc9eb6a2b13ebcfef5edf4c8aaeb8f8c6
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-06 14:23:56 -0500

    Update pivideostream.py

--- pivideostream.py
+++ pivideostream.py
@@ -31,6 +31,7 @@ class PiVideoStream:
 	def update(self):
 		self.stream = io.BytesIO()
 		self.camera.capture(self.stream, use_video_port=True, format='bgr')
+		self.stream.truncate()
 		self.stream.seek(0)
 		self.stream.readinto(self.rawCapture)
 		self.frame_buf_val = np.frombuffer(self.stream.getvalue(), dtype=np.uint8)
