491d65f51d5b0d0fc3a1c883880ad415029bbdf5
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-06 12:59:34 -0500

    Update threaded_cam.py

--- threaded_cam.py
+++ threaded_cam.py
@@ -69,8 +69,8 @@ last_tm = time.time()
 thread = PiVideoStream().start()
 try: 
 	while DISPLAY.loop_running():
-		#fps_txt.draw()
-		#ms_txt.draw()
+		fps_txt.draw()
+		ms_txt.draw()
 		#stream = io.BytesIO()
 		#start_ms = time.time() 
 		#camera.capture(stream, use_video_port=True, format='rgb', resize=(320, 320))
