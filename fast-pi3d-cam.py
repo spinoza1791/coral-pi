b5be8d83b7f336b4b97a12ae0628858c6023a276
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-07 16:40:47 -0500

    Update fast-pi3d-cam.py

--- fast-pi3d-cam.py
+++ fast-pi3d-cam.py
@@ -95,41 +95,41 @@ class ImageProcessor(threading.Thread):
         while not self.terminated:
             # Wait for an image to be written to the stream
             if self.event.wait(1):
-            try:
-                if self.stream.tell() >= NBYTES:
-                  #self.stream.readinto(self.rawCapture)
-                  bnp = np.array(self.stream.getbuffer(), dtype=np.uint8).reshape(mdl_dims * mdl_dims * 3)
-                  #self.input_val = np.frombuffer(self.stream.getvalue(), dtype=np.uint8)
-                  self.output = self.engine.DetectWithInputTensor(bnp, top_k=10)
-                  results = self.output
-                  #if self.output:
-                  #  num_obj = 0
-                  #  for obj in self.output:
-                  #    num_obj = num_obj + 1   
-                  #    buf = bbox.buf[0] # alias for brevity below
-                  #    buf.array_buffer[:,:3] = 0.0;
-                  #    for j, obj in enumerate(self.output):
-                  #      coords = (obj.bounding_box - 0.5) * [[1.0, -1.0]] * mdl_dims # broadcasting will fix the arrays size differences
-                  #      score = round(obj.score,2)
-                  #      ix = 8 * j
-                  #      buf.array_buffer[ix:(ix + 8), 0] = coords[X_IX, 0] + 2 * X_OFF
-                  #      buf.array_buffer[ix:(ix + 8), 1] = coords[Y_IX, 1] + 2 * Y_OFF
-                  #    buf.re_init(); # 
-                  #     bbox.draw() # i.e. one draw for all boxes
-                  #bnp = np.array(self.stream.getbuffer(),
-                  #              dtype=np.uint8).reshape(CAMH, CAMW, 3)
-                  #npa[:,:,0:3] = bnp
-                  new_pic = True
-            except Exception as e:
-              print(e)
-            finally:
-                # Reset the stream and event
-                self.stream.seek(0)
-                self.stream.truncate()
-                self.event.clear()
-                # Return ourselves to the pool
-                with lock:
-                    pool.append(self)
+              try:
+                  if self.stream.tell() >= NBYTES:
+                    #self.stream.readinto(self.rawCapture)
+                    bnp = np.array(self.stream.getbuffer(), dtype=np.uint8).reshape(mdl_dims * mdl_dims * 3)
+                    #self.input_val = np.frombuffer(self.stream.getvalue(), dtype=np.uint8)
+                    self.output = self.engine.DetectWithInputTensor(bnp, top_k=10)
+                    results = self.output
+                    #if self.output:
+                    #  num_obj = 0
+                    #  for obj in self.output:
+                    #    num_obj = num_obj + 1   
+                    #    buf = bbox.buf[0] # alias for brevity below
+                    #    buf.array_buffer[:,:3] = 0.0;
+                    #    for j, obj in enumerate(self.output):
+                    #      coords = (obj.bounding_box - 0.5) * [[1.0, -1.0]] * mdl_dims # broadcasting will fix the arrays size differences
+                    #      score = round(obj.score,2)
+                    #      ix = 8 * j
+                    #      buf.array_buffer[ix:(ix + 8), 0] = coords[X_IX, 0] + 2 * X_OFF
+                    #      buf.array_buffer[ix:(ix + 8), 1] = coords[Y_IX, 1] + 2 * Y_OFF
+                    #    buf.re_init(); # 
+                    #     bbox.draw() # i.e. one draw for all boxes
+                    #bnp = np.array(self.stream.getbuffer(),
+                    #              dtype=np.uint8).reshape(CAMH, CAMW, 3)
+                    #npa[:,:,0:3] = bnp
+                    new_pic = True
+              except Exception as e:
+                print(e)
+              finally:
+                  # Reset the stream and event
+                  self.stream.seek(0)
+                  self.stream.truncate()
+                  self.event.clear()
+                  # Return ourselves to the pool
+                  with lock:
+                      pool.append(self)
 
 def streams():
     while not done:
