c66f374cf26792de149dec4d2dd3e44d38bd2338
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-07 14:59:38 -0500

    Update create-envsphere-fast.py

--- create-envsphere-fast.py
+++ create-envsphere-fast.py
@@ -57,7 +57,7 @@ class ImageProcessor(threading.Thread):
 
     def run(self):
         # This method runs in a separate thread
-        global done, npa, new_pic, mdl_dims, NBYTES
+        global done, npa, new_pic, mdl_dims, NBYTES, bbox, X_OFF, Y_OFF, X_IX, Y_IX
         while not self.terminated:
             # Wait for an image to be written to the stream
             if self.event.wait(1):
