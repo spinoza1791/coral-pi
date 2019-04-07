2891ea45fc223c7f516657a819322ec273df00a3
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-07 14:04:45 -0500

    Update stream_thread.py

--- stream_thread.py
+++ stream_thread.py
@@ -28,12 +28,12 @@ pool = []
 
 class ImageProcessor(threading.Thread):
     def __init__(self):
-        super(ImageProcessor, self).__init__()
+		super(ImageProcessor, self).__init__()
 		self.rawCapture = bytearray(320 * 320 * 3)
-        self.stream = io.BytesIO()
-        self.event = threading.Event()
-        self.terminated = False
-        self.start()
+		self.stream = io.BytesIO()
+		self.event = threading.Event()
+		self.terminated = False
+		self.start()
 
     def run(self):
         # This method runs in a separate thread
