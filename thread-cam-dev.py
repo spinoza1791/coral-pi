3df8def65a7ecdf56117649678390d0dbe07db62
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-07 18:26:00 -0500

    Update thread-cam-dev.py

--- thread-cam-dev.py
+++ thread-cam-dev.py
@@ -67,7 +67,7 @@ class PiVideoStream:
 		self.frame_buf_val = np.frombuffer(self.stream.getvalue(), dtype=np.uint8)
 		self.output = self.engine.DetectWithInputTensor(self.frame_buf_val, top_k=10)
 		#results = self.output
-		self.stream.close()
+		#self.stream.close()
 		#for f in self.stream:
 		#	self.frame = io.BytesIO(f.array)
 		#	self.frame_buf_val = np.frombuffer(self.frame.getvalue(), dtype=np.uint8)
