276f5e0abd645a74917f1aed83ac2df3e7d95940
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-07 16:53:53 -0500

    Update fast-pi3d-cam.py

--- fast-pi3d-cam.py
+++ fast-pi3d-cam.py
@@ -36,38 +36,38 @@ preview_mid_Y = int(screen_H/2 - preview_H/2)
 max_obj = 10
 max_fps = 24
 
-DISPLAY = pi3d.Display.create(0, 0, w=preview_W, h=preview_H, layer=1, frames_per_second=max_fps)
-DISPLAY.set_background(0.0, 0.0, 0.0, 0.0) # transparent
-txtshader = pi3d.Shader("uv_flat")
-linshader = pi3d.Shader('mat_flat')
+#DISPLAY = pi3d.Display.create(0, 0, w=preview_W, h=preview_H, layer=1, frames_per_second=max_fps)
+#DISPLAY.set_background(0.0, 0.0, 0.0, 0.0) # transparent
+#txtshader = pi3d.Shader("uv_flat")
+#linshader = pi3d.Shader('mat_flat')
 
 # Fetch key presses
-keybd = pi3d.Keyboard()
-CAMERA = pi3d.Camera(is_3d=False)
-font = pi3d.Font("fonts/FreeMono.ttf", font_size=30, color=(0, 255, 0, 255)) # blue green 1.0 alpha
-elapsed_ms = 1000
-ms = str(elapsed_ms)
-ms_txt = pi3d.String(camera=CAMERA, is_3d=False, font=font, string=ms, x=0, y=preview_H/2 - 30, z=1.0)
-ms_txt.set_shader(txtshader)
-fps = "00.0 fps"
-N = 10
-fps_txt = pi3d.String(camera=CAMERA, is_3d=False, font=font, string=fps, x=0, y=preview_H/2 - 10, z=1.0)
-fps_txt.set_shader(txtshader)
-i = 0
-last_tm = time.time()
+#keybd = pi3d.Keyboard()
+#CAMERA = pi3d.Camera(is_3d=False)
+#font = pi3d.Font("fonts/FreeMono.ttf", font_size=30, color=(0, 255, 0, 255)) # blue green 1.0 alpha
+#elapsed_ms = 1000
+#ms = str(elapsed_ms)
+#ms_txt = pi3d.String(camera=CAMERA, is_3d=False, font=font, string=ms, x=0, y=preview_H/2 - 30, z=1.0)
+#ms_txt.set_shader(txtshader)
+####fps = "00.0 fps"
+#N = 10
+#fps_txt = pi3d.String(camera=CAMERA, is_3d=False, font=font, string=fps, x=0, y=preview_H/2 - 10, z=1.0)
+#fps_txt.set_shader(txtshader)
+#i = 0
+#last_tm = time.time()
 
-X_OFF = np.array([0, 0, -1, -1, 0, 0, 1, 1])
-Y_OFF = np.array([-1, -1, 0, 0, 1, 1, 0, 0])
-X_IX = np.array([0, 1, 1, 1, 1, 0, 0, 0])
-Y_IX = np.array([0, 0, 0, 1, 1, 1, 1, 0])
-verts = [[0.0, 0.0, 1.0] for i in range(8 * max_obj)] # need a vertex for each end of each side 
-bbox = pi3d.Lines(vertices=verts, material=(1.0,0.8,0.05), closed=False, strip=False, line_width=4) 
-bbox.set_shader(linshader)
-results = None
-old_results = None
-output = None
-frame_buf_val= None
-buf = None
+#X_OFF = np.array([0, 0, -1, -1, 0, 0, 1, 1])
+#Y_OFF = np.array([-1, -1, 0, 0, 1, 1, 0, 0])
+#X_IX = np.array([0, 1, 1, 1, 1, 0, 0, 0])
+#Y_IX = np.array([0, 0, 0, 1, 1, 1, 1, 0])
+#verts = [[0.0, 0.0, 1.0] for i in range(8 * max_obj)] # need a vertex for each end of each side 
+#bbox = pi3d.Lines(vertices=verts, material=(1.0,0.8,0.05), closed=False, strip=False, line_width=4) 
+#bbox.set_shader(linshader)
+#results = None
+#old_results = None
+#output = None
+#frame_buf_val= None
+#buf = None
 
 ########################################################################
 
@@ -93,6 +93,11 @@ class ImageProcessor(threading.Thread):
     def run(self):
         # This method runs in a separate thread
         global done, npa, new_pic, mdl_dims, NBYTES
+        DISPLAY = pi3d.Display.create(0, 0, w=preview_W, h=preview_H, layer=1, frames_per_second=max_fps)
+        DISPLAY.set_background(0.0, 0.0, 0.0, 0.0) # transparent
+        txtshader = pi3d.Shader("uv_flat")
+        linshader = pi3d.Shader('mat_flat')
+        CAMERA = pi3d.Camera(is_3d=False)
         X_OFF = np.array([0, 0, -1, -1, 0, 0, 1, 1])
         Y_OFF = np.array([-1, -1, 0, 0, 1, 1, 0, 0])
         X_IX = np.array([0, 1, 1, 1, 1, 0, 0, 0])
@@ -100,44 +105,45 @@ class ImageProcessor(threading.Thread):
         verts = [[0.0, 0.0, 1.0] for i in range(8 * max_obj)] # need a vertex for each end of each side 
         bbox = pi3d.Lines(vertices=verts, material=(1.0,0.8,0.05), closed=False, strip=False, line_width=4) 
         bbox.set_shader(linshader)
-        while not self.terminated:
-            # Wait for an image to be written to the stream
-            if self.event.wait(1):
-              try:
-                  if self.stream.tell() >= NBYTES:
-                    #self.stream.readinto(self.rawCapture)
-                    bnp = np.array(self.stream.getbuffer(), dtype=np.uint8).reshape(mdl_dims * mdl_dims * 3)
-                    #self.input_val = np.frombuffer(self.stream.getvalue(), dtype=np.uint8)
-                    self.output = self.engine.DetectWithInputTensor(bnp, top_k=10)
-                    results = self.output
-                    if self.output:
-                      num_obj = 0
-                      for obj in self.output:
-                        num_obj = num_obj + 1   
-                        buf = bbox.buf[0] # alias for brevity below
-                        buf.array_buffer[:,:3] = 0.0;
-                        for j, obj in enumerate(self.output):
-                          coords = (obj.bounding_box - 0.5) * [[1.0, -1.0]] * mdl_dims # broadcasting will fix the arrays size differences
-                          score = round(obj.score,2)
-                          ix = 8 * j
-                          buf.array_buffer[ix:(ix + 8), 0] = coords[X_IX, 0] + 2 * X_OFF
-                          buf.array_buffer[ix:(ix + 8), 1] = coords[Y_IX, 1] + 2 * Y_OFF
-                        buf.re_init(); # 
-                        bbox.draw() # i.e. one draw for all boxes
-                    #bnp = np.array(self.stream.getbuffer(),
-                    #              dtype=np.uint8).reshape(CAMH, CAMW, 3)
-                    #npa[:,:,0:3] = bnp
-                    new_pic = True
-              except Exception as e:
-                print(e)
-              finally:
-                  # Reset the stream and event
-                  self.stream.seek(0)
-                  self.stream.truncate()
-                  self.event.clear()
-                  # Return ourselves to the pool
-                  with lock:
-                      pool.append(self)
+        if DISPLAY.loop_running():
+          while not self.terminated:
+              # Wait for an image to be written to the stream
+              if self.event.wait(1):
+                try:
+                    if self.stream.tell() >= NBYTES:
+                      #self.stream.readinto(self.rawCapture)
+                      bnp = np.array(self.stream.getbuffer(), dtype=np.uint8).reshape(mdl_dims * mdl_dims * 3)
+                      #self.input_val = np.frombuffer(self.stream.getvalue(), dtype=np.uint8)
+                      self.output = self.engine.DetectWithInputTensor(bnp, top_k=10)
+                      results = self.output
+                      if self.output:
+                        num_obj = 0
+                        for obj in self.output:
+                          num_obj = num_obj + 1   
+                          buf = bbox.buf[0] # alias for brevity below
+                          buf.array_buffer[:,:3] = 0.0;
+                          for j, obj in enumerate(self.output):
+                            coords = (obj.bounding_box - 0.5) * [[1.0, -1.0]] * mdl_dims # broadcasting will fix the arrays size differences
+                            score = round(obj.score,2)
+                            ix = 8 * j
+                            buf.array_buffer[ix:(ix + 8), 0] = coords[X_IX, 0] + 2 * X_OFF
+                            buf.array_buffer[ix:(ix + 8), 1] = coords[Y_IX, 1] + 2 * Y_OFF
+                          buf.re_init(); # 
+                          bbox.draw() # i.e. one draw for all boxes
+                      #bnp = np.array(self.stream.getbuffer(),
+                      #              dtype=np.uint8).reshape(CAMH, CAMW, 3)
+                      #npa[:,:,0:3] = bnp
+                      new_pic = True
+                except Exception as e:
+                  print(e)
+                finally:
+                    # Reset the stream and event
+                    self.stream.seek(0)
+                    self.stream.truncate()
+                    self.event.clear()
+                    # Return ourselves to the pool
+                    with lock:
+                        pool.append(self)
 
 def streams():
     while not done:
@@ -171,18 +177,18 @@ t.start()
 while not new_pic:
     time.sleep(1)
 
-while DISPLAY.loop_running():
-    fps_txt.draw()   
+#while DISPLAY.loop_running():
+    #fps_txt.draw()   
     #ms_txt.draw()
     #ms = str(elapsed_ms*1000)
     #ms_txt.quick_change(ms)
-    i += 1
-    if i > N:
-        tm = time.time()
-        fps = "{:5.1f}FPS".format(i / (tm - last_tm))
-        fps_txt.quick_change(fps)
-        i = 0
-        last_tm = tm
+    #i += 1
+    #if i > N:
+    #    tm = time.time()
+    #    fps = "{:5.1f}FPS".format(i / (tm - last_tm))
+    #    fps_txt.quick_change(fps)
+    #    i = 0
+    #    last_tm = tm
     #if results:
     #  num_obj = 0
     #  for obj in results:
@@ -197,10 +203,10 @@ while DISPLAY.loop_running():
     #     buf.array_buffer[ix:(ix + 8), 1] = coords[Y_IX, 1] + 2 * Y_OFF
     #    buf.re_init(); # 
     #    bbox.draw() # i.e. one draw for all boxes
-    if keybd.read() == 27:
-      keybd.close()
-      DISPLAY.destroy()
-      break
+    #if keybd.read() == 27:
+    #  keybd.close()
+    #  DISPLAY.destroy()
+    #  break
 
 # Shut down the processors in an orderly fashion
 while pool:
