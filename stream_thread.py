90cf316dd829c3d77ca051b14c0cbdfbc15c6928
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-07 20:04:15 -0500

    Update stream_thread.py

--- stream_thread.py
+++ stream_thread.py
@@ -133,9 +133,34 @@ with picamera.PiCamera() as camera:
 	time.sleep(2)
 	camera.capture_sequence(streams(), use_video_port=True)
 	while DISPLAY.loop_running():
+		print("display running")
+		fps_txt.draw()
+		ms_txt.draw()
+		ms = str(elapsed_ms*1000)
+		ms_txt.quick_change(ms)
+		i += 1
+		if i > N:
+			tm = time.time()
+			fps = "{:5.1f}FPS".format(i / (tm - last_tm))
+			fps_txt.quick_change(fps)
+			i = 0
+			last_tm = tm
+		#results = thread.read()
+		if results:
+			num_obj = 0
+			for obj in results:
+				num_obj = num_obj + 1   
+				buf = bbox.buf[0] # alias for brevity below
+				buf.array_buffer[:,:3] = 0.0;
+				for j, obj in enumerate(results):
+					coords = (obj.bounding_box - 0.5) * [[1.0, -1.0]] * mdl_dims # broadcasting will fix the arrays size differences
+					score = round(obj.score,2)
+					ix = 8 * j
+					buf.array_buffer[ix:(ix + 8), 0] = coords[X_IX, 0] + 2 * X_OFF
+					buf.array_buffer[ix:(ix + 8), 1] = coords[Y_IX, 1] + 2 * Y_OFF
+				buf.re_init(); # 
+				bbox.draw() # i.e. one draw for all boxes
 		if keybd.read() == 27:
-			keybd.close()
-			camera.close()
 			break
 
 # Shut down the processors in an orderly fashion
