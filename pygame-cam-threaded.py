3f187aecf92822bc59ef2b61f62821c7b7f7d33c
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-08 16:18:06 -0500

    Update pygame-cam-threaded.py

--- pygame-cam-threaded.py
+++ pygame-cam-threaded.py
@@ -36,14 +36,6 @@ preview_mid_Y = int(screen_H/2 - preview_H/2)
 
 #engine = edgetpu.detection.engine.DetectionEngine(args.model)
 os.environ['SDL_VIDEO_CENTERED'] = '1'
-pygame.init()
-pygame.display.set_caption('Face Detection')
-screen = pygame.display.set_mode((mdl_dims, mdl_dims), pygame.DOUBLEBUF|pygame.HWSURFACE)
-pygame.font.init()
-fnt_sz = 18
-myfont = pygame.font.SysFont('Arial', fnt_sz)
-results = None
-
 
 ########################################################################
 
@@ -109,8 +101,20 @@ def streams():
 
 def start_capture(): # has to be in yet another thread as blocking
 	global mdl_dims, pool, results, screen, start_ms, elapsed_ms, fnt_sz, preview_mid_X, preview_mid_Y
+	x1, x2, x3, x4, x5 = 0, 50, 50, 0, 0
+	y1, y2, y3, y4, y5 = 50, 50, 0, 0, 50
+	z = 5
+	last_tm = time.time()
+	i = 0
+	results = None
 	with picamera.PiCamera() as camera:
 		pool = [ImageProcessor() for i in range(4)]
+		pygame.init()
+		pygame.display.set_caption('Face Detection')
+		screen = pygame.display.set_mode((mdl_dims, mdl_dims), pygame.DOUBLEBUF|pygame.HWSURFACE)
+		pygame.font.init()
+		fnt_sz = 18
+		myfont = pygame.font.SysFont('Arial', fnt_sz)
 		camera.resolution = (mdl_dims, mdl_dims)
 		rgb = bytearray(camera.resolution[0] * camera.resolution[1] * 3)
 		camera.framerate = 24
@@ -121,15 +125,15 @@ def start_capture(): # has to be in yet another thread as blocking
 			  (camera.resolution[0] * camera.resolution[1] * 3)],
 			   camera.resolution, 'RGB')
 		if img:
-		 screen.blit(img, (0,0))
-		 if results:
+			screen.blit(img, (0,0))
+			if results:
 			  num_obj = 0
 			  for obj in results:
 				   num_obj = num_obj + 1
 			  for obj in results:
 				   bbox = obj.bounding_box.flatten().tolist()
 				   score = round(obj.score,2)
-				   x1 = round(bbox[0] * mdl_dims)
+			   x1 = round(bbox[0] * mdl_dims)
 				   y1 = round(bbox[1] * mdl_dims)
 				   x2 = round(bbox[2] * mdl_dims)
 				   y2 = round(bbox[3] * mdl_dims)
@@ -145,7 +149,7 @@ def start_capture(): # has to be in yet another thread as blocking
 				   screen.blit(fnt_ms,((mdl_dims / 2) - (fnt_ms_width / 2), 0))
 				   bbox_rect = pygame.draw.rect(screen, (0,0,255), (x1, y1, rect_width, rect_height), 2)
 				   #pygame.display.update(bbox_rect)
-		 else:
+			else:
 			  elapsed_ms = time.time() - start_ms
 			  ms = "%s %.2fms" % ("No faces detected in", elapsed_ms*1000)
 			  fnt_ms = myfont.render(ms, True, (255,0,0))
@@ -160,26 +164,30 @@ t.start()
 while not new_pic:
     time.sleep(0.1)
 
+# Shut down the processors in an orderly fashion
+while pool:
+	done = True
+	with lock:
+		processor = pool.pop()
+		processor.terminated = True
+		processor.join()
+
 #camera = picamera.PiCamera()
 #Set camera resolution equal to model dims
 #camera.resolution = (mdl_dims, mdl_dims)
 #rgb = bytearray(camera.resolution[0] * camera.resolution[1] * 3)
 #camera.framerate = 40
 #_, width, height, channels = engine.get_input_tensor_shape()
-x1, x2, x3, x4, x5 = 0, 50, 50, 0, 0
-y1, y2, y3, y4, y5 = 50, 50, 0, 0, 50
-z = 5
-last_tm = time.time()
-i = 0
 
-exitFlag = True
-while(exitFlag):
-	for event in pygame.event.get():
-		keys = pygame.key.get_pressed()
-		if(keys[pygame.K_ESCAPE] == 1):
-			pygame.display.quit()
-			camera.close()
-			exitFlag = False
+
+#exitFlag = True
+#while(exitFlag):
+#	for event in pygame.event.get():
+#		keys = pygame.key.get_pressed()
+#		if(keys[pygame.K_ESCAPE] == 1):
+#			pygame.display.quit()
+#			camera.close()
+#			exitFlag = False
     #with picamera.array.PiRGBArray(camera, size=(mdl_dims, mdl_dims)) as stream:        
         #stream = io.BytesIO()
         #start_ms = time.time()
