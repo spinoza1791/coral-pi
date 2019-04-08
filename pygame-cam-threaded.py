0613471d9a532cf07b7a099f846fb3cecfcf8652
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-08 16:20:28 -0500

    Update pygame-cam-threaded.py

--- pygame-cam-threaded.py
+++ pygame-cam-threaded.py
@@ -127,34 +127,34 @@ def start_capture(): # has to be in yet another thread as blocking
 		if img:
 			screen.blit(img, (0,0))
 			if results:
-			  num_obj = 0
-			  for obj in results:
-				   num_obj = num_obj + 1
-			  for obj in results:
-				   bbox = obj.bounding_box.flatten().tolist()
-				   score = round(obj.score,2)
-			   x1 = round(bbox[0] * mdl_dims)
-				   y1 = round(bbox[1] * mdl_dims)
-				   x2 = round(bbox[2] * mdl_dims)
-				   y2 = round(bbox[3] * mdl_dims)
-				   rect_width = x2 - x1
-				   rect_height = y2 - y1
-				   class_score = "%.2f" % (score)
-				   ms = "(%d) %s%.2fms" % (num_obj, "faces detected in ", elapsed_ms*1000)
-				   fnt_class_score = myfont.render(class_score, True, (0,0,255))
-				   fnt_class_score_width = fnt_class_score.get_rect().width
-				   screen.blit(fnt_class_score,(x1, y1-fnt_sz))
-				   fnt_ms = myfont.render(ms, True, (255,255,255))
-				   fnt_ms_width = fnt_ms.get_rect().width
-				   screen.blit(fnt_ms,((mdl_dims / 2) - (fnt_ms_width / 2), 0))
-				   bbox_rect = pygame.draw.rect(screen, (0,0,255), (x1, y1, rect_width, rect_height), 2)
-				   #pygame.display.update(bbox_rect)
+				num_obj = 0
+				for obj in results:
+					num_obj = num_obj + 1
+				for obj in results:
+					bbox = obj.bounding_box.flatten().tolist()
+					score = round(obj.score,2)
+					x1 = round(bbox[0] * mdl_dims)
+					y1 = round(bbox[1] * mdl_dims)
+					x2 = round(bbox[2] * mdl_dims)
+					y2 = round(bbox[3] * mdl_dims)
+					rect_width = x2 - x1
+					rect_height = y2 - y1
+					class_score = "%.2f" % (score)
+					ms = "(%d) %s%.2fms" % (num_obj, "faces detected in ", elapsed_ms*1000)
+					fnt_class_score = myfont.render(class_score, True, (0,0,255))
+					fnt_class_score_width = fnt_class_score.get_rect().width
+					screen.blit(fnt_class_score,(x1, y1-fnt_sz))
+					fnt_ms = myfont.render(ms, True, (255,255,255))
+					fnt_ms_width = fnt_ms.get_rect().width
+					screen.blit(fnt_ms,((mdl_dims / 2) - (fnt_ms_width / 2), 0))
+					bbox_rect = pygame.draw.rect(screen, (0,0,255), (x1, y1, rect_width, rect_height), 2)
+					#pygame.display.update(bbox_rect)
 			else:
-			  elapsed_ms = time.time() - start_ms
-			  ms = "%s %.2fms" % ("No faces detected in", elapsed_ms*1000)
-			  fnt_ms = myfont.render(ms, True, (255,0,0))
-			  fnt_ms_width = fnt_ms.get_rect().width
-			  screen.blit(fnt_ms,((mdl_dims / 2) - (fnt_ms_width / 2), 0))
+				elapsed_ms = time.time() - start_ms
+				ms = "%s %.2fms" % ("No faces detected in", elapsed_ms*1000)
+				fnt_ms = myfont.render(ms, True, (255,0,0))
+				fnt_ms_width = fnt_ms.get_rect().width
+				screen.blit(fnt_ms,((mdl_dims / 2) - (fnt_ms_width / 2), 0))
 		pygame.display.update()
 
 
