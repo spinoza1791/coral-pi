64ad54df72a437d6bbcdc22474e62ad80977c360
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-09 18:23:11 -0500

    Update face_pygame.py

--- face_pygame.py
+++ face_pygame.py
@@ -69,7 +69,7 @@ def main():
 	while True:
 		img = cam.get_image()
 		img = pygame.transform.scale(img,(320,320))
-		img_arr = pygame.surfarray.array3d(img)
+		#img_arr = pygame.surfarray.array3d(img)
 		screen.blit(img, (0,0))
 		#img_io.seek(0)
 		#img.readinto(rgb)
@@ -79,7 +79,7 @@ def main():
 		#img_frame.readinto(rgb)
 		#input = np.frombuffer(img_arr.getvalue(), dtype=np.uint8)
 		#Inference
-		results = engine.DetectWithInputTensor(img_arr, top_k=max_obj)
+		results = engine.DetectWithInputTensor(img, top_k=max_obj)
 		#stream.close()                                                                 
 		#if img:
 			#screen.blit(img, (0,0))
