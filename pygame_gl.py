c1094d7f9a5a58dbca5523438a75d0c974d7e4d5
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-09 13:17:44 -0500

    Update pygame_gl.py

--- pygame_gl.py
+++ pygame_gl.py
@@ -1,126 +1,64 @@
-#!/usr/bin/env python3
+import pygame
+from pygame.locals import *
 
-import pygame.locals
-import OpenGL.GLU
-import OpenGL.GL
+from OpenGL.GL import *
+from OpenGL.GLU import *
 
-def Cube(rotatex, rotatey, dist, w, h):
-    """Define the basic parts of a 2x2x2 cube"""
-    corners= (
-        (1, -1, -1),
-        (1, 1, -1),
-        (-1, 1, -1),
-        (-1, -1, -1),
-        (1, -1, 1),
-        (1, 1, 1),
-        (-1, -1, 1),
-        (-1, 1, 1)
-        )
+verticies = (
+    (1, -1, -1),
+    (1, 1, -1),
+    (-1, 1, -1),
+    (-1, -1, -1),
+    (1, -1, 1),
+    (1, 1, 1),
+    (-1, -1, 1),
+    (-1, 1, 1)
+    )
 
-    edges = (
-        (0,1),
-        (0,3),
-        (0,4),
-        (2,1),
-        (2,3),
-        (2,7),
-        (6,3),
-        (6,4),
-        (6,7),
-        (5,1),
-        (5,4),
-        (5,7)
-        )
+edges = (
+    (0,1),
+    (0,3),
+    (0,4),
+    (2,1),
+    (2,3),
+    (2,7),
+    (6,3),
+    (6,4),
+    (6,7),
+    (5,1),
+    (5,4),
+    (5,7)
+    )
 
-    faces = (
-        (0,1,2,3),
-        (3,2,7,6),
-        (6,7,5,4),
-        (4,5,1,0),
-        (1,5,7,2),
-        (4,0,3,6)
-        )
 
-    colors = [  (1,0,0),
-        (0,1,0),
-        (0,0,1),
-        (1,1,0),
-        (1,0,1),
-        (0,1,1)] #To easily switch colors between drawing faces.
-
-    OpenGL.GL.glLineWidth(5.)
-    """
-    There is about 0 configuration you can do in between glBegin and glEnd.
-    If you want to define the line width of your edges, or the culling of your faces... do it before glBegin.
-    This also means it is a big PITA to get just one of the edges to be more broad. You'll have to start two instances of GL_LINES.
-    """
-    OpenGL.GL.glMatrixMode(OpenGL.GL.GL_PROJECTION)
-    OpenGL.GL.glPushMatrix()
-    OpenGL.GL.glLoadIdentity()
-    OpenGL.GLU.gluPerspective(45, (w / h), 1, 10)
-    OpenGL.GL.glMatrixMode(OpenGL.GL.GL_MODELVIEW)
-    OpenGL.GL.glPushMatrix()
-    OpenGL.GL.glLoadIdentity()
-    OpenGL.GL.glTranslatef(0.0,0.0, dist) #The cube is drawn around the origin. Move the camera back a little.
-    OpenGL.GL.glRotatef(rotatex, 1, 0, 0)
-    OpenGL.GL.glRotatef(rotatey, 0, 1, 0) #Rotate every frame. The first value is the angle which is rotated (read: rotation speed), values 2-4 give a 3D vector / axis to rotate about.
-    OpenGL.GL.glBegin(OpenGL.GL.GL_LINES) #You'll have to define what kind of object you are drawing. Polygons? Quadrilaterals? Lines? They all start their own drawing instance it seems.
-    OpenGL.GL.glColor3fv((1,1,1)) #This is an exception! You can define color in between glBegin and glEnd!
+def Cube():
+    glBegin(GL_LINES)
     for edge in edges:
         for vertex in edge:
-            OpenGL.GL.glVertex3fv(corners[vertex]) #Draws a line (we're in GL_LINES mode) in between the two 3D-tuples that are called in separate instances of this function. e.g. You'll have to call this function at least twice to draw something.
-    OpenGL.GL.glEnd()
-
-
-    OpenGL.GL.glCullFace(OpenGL.GL.GL_FRONT); #Only draw the part that faces the camera.
-    OpenGL.GL.glEnable(OpenGL.GL.GL_CULL_FACE); #Only draw part of the objects
-    OpenGL.GL.glBegin(OpenGL.GL.GL_QUADS)
-    i=0
-    for surface in faces:
-        OpenGL.GL.glColor3fv(colors[i])
-        i+=1 #swap colors.
-        for vertex in surface:
-            OpenGL.GL.glVertex3fv(corners[vertex]) #Will only draw one quadrilateral every 4 times you call this function with a tuple in 3D space.
-    OpenGL.GL.glEnd()
-    OpenGL.GL.glPopMatrix()
-    OpenGL.GL.glMatrixMode(OpenGL.GL.GL_PROJECTION)
-    OpenGL.GL.glPopMatrix()
-    glEnable(GL_TEXTURE_2D)).
-
+            glVertex3fv(verticies[vertex])
+    glEnd()
 
 
 def main():
     pygame.init()
-    rotatex, rotatey, dist = 0, 0, -5.
-    w, h = 320, 320
-    display = (w,h) #Tiniest screen resolution as window size.
-    pygame.display.set_mode(display, pygame.locals.DOUBLEBUF|pygame.OPENGL)
-    OpenGL.GLU.gluPerspective(45, (display[0]/display[1]), 1, 20)
+    display = (800,600)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
 
-    OpenGL.GL.glTranslatef(0.0,0.0, -5) #The cube is drawn around the origin. Move the camera back a little.
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
 
-    # pygame.key.set_repeat(1,10)
-    xspeed, yspeed, radspeed = 0, 0, 0
+    glTranslatef(0.0,0.0, -5)
 
     while True:
-        for event in pygame.event.get(): #This is getting the basic listeners.
-            if event.type == pygame.QUIT: #What to do if the close button is pressed
-                pygame.quit() #Stop the game!
-                quit() #Stop the python!
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
 
-        # print(OpenGL.GL.glGetFloatv())
-        OpenGL.GL.glClear(OpenGL.GL.GL_COLOR_BUFFER_BIT|OpenGL.GL.GL_DEPTH_BUFFER_BIT) #Make sure it doesn't draw anything where there's not supposed to be anything. Remove it for sticky pixels.
-        Cube(rotatex, rotatey, dist, w, h) #Actually draw the fucking cube. Is defined above.
-        font = pygame.font.Font (None, 64)
-        textSurface = font.render("Je moeder", False, (255,255,255,128), (0,0,0,0))
-        textData = pygame.image.tostring(textSurface, "RGBA", True)
-        # print(textData)
-        OpenGL.GL.glEnable(OpenGL.GL.GL_BLEND)
-        OpenGL.GL.glBlendFunc(OpenGL.GL.GL_ONE, OpenGL.GL.GL_SRC_ALPHA)
-        OpenGL.GL.glRasterPos3d(-2.75,-2.0625,0)
-        OpenGL.GL.glDrawPixels(textSurface.get_width(), textSurface.get_height(), OpenGL.GL.GL_RGBA, OpenGL.GL.GL_UNSIGNED_BYTE, textData)
-        OpenGL.GL.glDisable(OpenGL.GL.GL_BLEND)
-        pygame.display.flip() #Updates the display with new contents.
-        pygame.time.wait(10) #Wait 10 miliseconds per frame for a 100fps.
+        glRotatef(1, 3, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        Cube()
+        pygame.display.flip()
+        pygame.time.wait(10)
+
 
 main()
