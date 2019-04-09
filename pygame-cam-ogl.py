c447aa86d7896b55adbc32488ad500f67b2b7870
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-09 14:05:31 -0500

    Create pygame-cam-ogl.py

---
+++ pygame-cam-ogl.py
@@ -0,0 +1,28 @@
+
+import sys
+import pygame
+import pygame.camera
+import os
+
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+
+pygame.init()
+pygame.camera.init()
+
+screen = pygame.display.set_mode((320,320),0)
+cam_list = pygame.camera.list_cameras()
+cam = pygame.camera.Camera(cam_list[0],(32,24))
+cam.start()
+
+while True:
+   image1 = cam.get_image()
+   image1 = pygame.transform.scale(image1,(320,320))
+   screen.blit(image1,(0,0))
+   pygame.display.update()
+
+    exitFlag = True
+    while(exitFlag):
+        for event in pygame.event.get():
+            keys = pygame.key.get_pressed()
+            if(keys[pygame.K_ESCAPE] == 1):
+                exitFlag = False   
