002e1cae4b388777176bb212f48f8970d0409940
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-09 13:56:09 -0500

    Update pygame_gl.py

--- pygame_gl.py
+++ pygame_gl.py
@@ -84,7 +84,7 @@ def main():
         #glRotatef(1, 3, 1, 1)
         #glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         #Cube()
-        pygame.display.flip()
+        pygame.display.update()
         #pygame.time.wait(10)
 
 
