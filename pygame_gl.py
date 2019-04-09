6d72924228e350af2c326602759f26f64da703b6
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-09 13:40:21 -0500

    Update pygame_gl.py

--- pygame_gl.py
+++ pygame_gl.py
@@ -1,3 +1,11 @@
+import argparse
+import io
+import time
+import numpy as np
+import picamera
+import picamera.array
+from PIL import Image
+import edgetpu.detection.engine
 import pygame
 from pygame.locals import *
 
@@ -40,20 +48,35 @@ def Cube():
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+    '--model', help='File path of Tflite model.', required=False)
+    parser.add_argument(
+    '--dims', help='Model input dimension', required=False)
+    args = parser.parse_args()
+    mdl_dims = int(args.dims)
+    
     pygame.init()
-    display = (800,600)
-    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
-
+    #display = (800,600)
+    #pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+    display = pygame.display.set_mode((mdl_dims, mdl_dims), DOUBLEBUF|OPENGL)
+    camera = picamera.PiCamera()
+    camera.resolution = (mdl_dims, mdl_dims)
+    
     gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
-
     glTranslatef(0.0,0.0, -5)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                quit()
-
+                quit()            
+            
+        img = pygame.image.frombuffer(rgb[0:
+        (camera.resolution[0] * camera.resolution[1] * 3)],
+        camera.resolution, 'RGB')
+        if img:
+            display.blit(img, (mdl_dims,mdl_dims))
         glRotatef(1, 3, 1, 1)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         Cube()
