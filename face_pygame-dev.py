dd666336bcae2bab158582842a9638a522b0ff47
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-10 14:39:35 -0500

    Update face_pygame-dev.py

--- face_pygame-dev.py
+++ face_pygame-dev.py
@@ -36,7 +36,7 @@ def main():
 	pygame.init()
 	pygame.camera.init()
 	#screen = pygame.display.set_mode((mdl_dims, mdl_dims), pygame.DOUBLEBUF|pygame.HWSURFACE)
-	screen = pygame.display.set_mode((320,320),pygame.DOUBLEBUF|pygame.HWSURFACE)
+	screen = pygame.display.set_mode((320,320), 0) #,pygame.DOUBLEBUF|pygame.HWSURFACE)
 	##pygame.display.set_caption('Face Detection')
 	pycam = pygame.camera.Camera("/dev/video0",(480,480))
 	pycam.start()
