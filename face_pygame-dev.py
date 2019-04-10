cd1c5485556d9640697cf56e1e755057500dd916
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-10 17:03:25 -0500

    Update face_pygame-dev.py

--- face_pygame-dev.py
+++ face_pygame-dev.py
@@ -114,8 +114,8 @@ def main():
 		elapsed_ms = time.time() - start_ms
 
 		#if img:
-		#	screen.blit(img, (0,0))
-		pygame.surfarray.blit_array(screen, img_arr)
+		screen.blit(img, (0,0))
+		#pygame.surfarray.blit_array(screen, img_arr)
 	
 		i += 1
 		if i > N:
