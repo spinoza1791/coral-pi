ce32eab4f1dcd40802c777a6c7892ded242514cb
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-10 14:32:09 -0500

    Update face_pygame-dev.py

--- face_pygame-dev.py
+++ face_pygame-dev.py
@@ -93,14 +93,16 @@ def main():
 		img = pycam.get_image()
 		img = pygame.transform.scale(img,(320,320))
 		#img = pygame.transform.rotate(img, 90)
-		img_arr = pygame.surfarray.array3d(img)
+		#img_arr = pygame.surfarray.array3d(img)
+		img_arr = pygame.surfarray.pixels3d(img)
+		img_arr = np.ascontiguousarray(img_arr)
 		start_ms = time.time()
 		#for f in img_arr:
 		frame = io.BytesIO(img_arr)
 		frame_buf_val = np.frombuffer(frame.getvalue(), dtype=np.uint8)
-		results = engine.DetectWithInputTensor(frame_buf_val, top_k=10)
 		print(frame_buf_val)
-		frame.truncate(0)
+		results = engine.DetectWithInputTensor(frame_buf_val, top_k=10)
+		#frame.truncate(0)
 		elapsed_ms = time.time() - start_ms
 
 		if img:
