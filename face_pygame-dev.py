1e3d41fad7c9e29f2d989dc123572c90ac813567
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-10 14:36:51 -0500

    Update face_pygame-dev.py

--- face_pygame-dev.py
+++ face_pygame-dev.py
@@ -95,7 +95,8 @@ def main():
 		#img = pygame.transform.rotate(img, 90)
 		#img_arr = pygame.surfarray.array3d(img)
 		img_arr = pygame.surfarray.pixels3d(img)
-		img_arr = np.rot90(img_arr)
+		#img_arr = np.rot90(img_arr)
+		img_arr = np.rot90(img_arr, k=1, axes=(1,0)) 
 		img_arr = np.ascontiguousarray(img_arr)
 		start_ms = time.time()
 		#for f in img_arr:
