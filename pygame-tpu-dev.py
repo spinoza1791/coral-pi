021171e316a21809f38527164f646ead9a82b0b9
ANDREW CRATON <andrew.craton@gmail.com>
2019-04-10 20:32:25 -0500

    Update pygame-tpu-dev.py

--- pygame-tpu-dev.py
+++ pygame-tpu-dev.py
@@ -135,7 +135,7 @@ def main():
 
 		fps_fnt = fnt.render(fps, True, (255,255,0))
 		fps_width = fps_fnt.get_rect().width
-		screen.blit(fps_fnt,((mdl_dims / 2 * sz_x) - (fps_width / 2), 20))
+		screen.blit(fps_fnt,((mdl_dims / 2 * sz_x) - (fps_width / 2 * sz_y), 20))
 		
 		if results:
 			num_obj = 0
@@ -154,8 +154,8 @@ def main():
 				y1 = round(bbox[0] * mdl_dims * sz_y) 
 				x2 = round(bbox[0] * mdl_dims * sz_x) 
 				y2 = round(bbox[0] * mdl_dims * sz_y) 
-				rect_width = round((x2 - x1) * sz_x)
-				rect_height = round((y2 - y1) * sz_y)
+				rect_width = (x2 - x1)
+				rect_height = (y2 - y1)
 				#class_score = "%.2f" % (score)
 				#fnt_class_score = fnt.render(class_score, True, (0,0,255))
 				#fnt_class_score_width = fnt_class_score.get_rect().width
@@ -163,14 +163,14 @@ def main():
 				ms = "(%d) %s%.2fms" % (num_obj, "objects detected in ", elapsed_ms*1000)
 				fnt_ms = fnt.render(ms, True, (255,255,255))
 				fnt_ms_width = fnt_ms.get_rect().width
-				screen.blit(fnt_ms,((mdl_dims / 2 * sz_x) - (fnt_ms_width / 2), 0))
+				screen.blit(fnt_ms,((mdl_dims / 2 * sz_x) - (fnt_ms_width / 2 * sz_y), 0))
 				bbox_rect = pygame.draw.rect(screen, (0,255,0), (x1, y1, rect_width, rect_height), 4)
 
 		else:
 			ms = "%s %.2fms" % ("No objects detected in", elapsed_ms*1000)
 			fnt_ms = fnt.render(ms, True, (255,0,0))
 			fnt_ms_width = fnt_ms.get_rect().width
-			screen.blit(fnt_ms,((mdl_dims / 2 * sz_x) - (fnt_ms_width / 2), 0))
+			screen.blit(fnt_ms,((mdl_dims / 2 * sz_x) - (fnt_ms_width / 2 * sz_y), 0))
 
 		for event in pygame.event.get():
 			keys = pygame.key.get_pressed()
